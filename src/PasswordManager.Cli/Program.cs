50de233ccd02dc7124c56a53e859e5d1564f6d00
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-03-16 00:48:49 +0200

    removed sqlite db

--- src/PasswordManager.Cli/Program.cs
+++ src/PasswordManager.Cli/Program.cs
@@ -7,20 +7,20 @@ CommandApp app = new();
 
 app.Configure(config =>
 {
-    config.SetApplicationName("passman");
-    config.SetApplicationVersion("v1.0.0");
+    _ = config.SetApplicationName("passman");
+    _ = config.SetApplicationVersion("v1.0.0");
 
 
-    config.AddCommand<SavePasswordCommand>("add")
+    _ = config.AddCommand<SavePasswordCommand>("add")
     .WithAlias("-d")
     .WithDescription("Add new Password")
     .WithExample(new[] { "add", "nicksoftware.co.za", "nick", "P@ssw0rd!" });
 
-    config.AddCommand<GetPasswordCommand>("get")
+    _ = config.AddCommand<GetPasswordCommand>("get")
         .WithDescription("Get an existing password")
         .WithExample(new[] { "get", "nick.co.za" });
 
-    config.AddCommand<GeneratePasswordCommand>("generate")
+    _ = config.AddCommand<GeneratePasswordCommand>("generate")
         .WithAlias("-g")
         .WithDescription("Get an existing password")
         .WithExample(new[] { "generate", "nick.co.za" });
