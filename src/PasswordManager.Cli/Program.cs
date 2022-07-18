8f126e41fdb3cfdc4ff3381ac2d08fc440779325
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-07-18 10:34:31 +0200

    Update Program.cs

--- src/PasswordManager.Cli/Program.cs
+++ src/PasswordManager.Cli/Program.cs
@@ -6,20 +6,21 @@ CommandApp app = new();
 
 app.Configure(config =>
 {
-    _ = config.SetApplicationName("passman");
-    _ = config.SetApplicationVersion("v1.0.0");
+    config.SetApplicationName("passman");
+    config.SetApplicationVersion("v1.0.0");
 
+    config
+        .AddCommand<SavePasswordCommand>("add")
+        .WithAlias("-d")
+        .WithDescription("Add new Password")
+        .WithExample(new[] { "add", "nicksoftware.co.za", "nick", "P@ssw0rd!" });
 
-    _ = config.AddCommand<SavePasswordCommand>("add")
-    .WithAlias("-d")
-    .WithDescription("Add new Password")
-    .WithExample(new[] { "add", "nicksoftware.co.za", "nick", "P@ssw0rd!" });
-
-    _ = config.AddCommand<GetPasswordCommand>("get")
+    config
+        .AddCommand<GetPasswordCommand>("get")
         .WithDescription("Get an existing password")
         .WithExample(new[] { "get", "nick.co.za" });
 
-    _ = config.AddCommand<GeneratePasswordCommand>("generate")
+    config.AddCommand<GeneratePasswordCommand>("generate")
         .WithAlias("-g")
         .WithDescription("Generate new password")
         .WithExample(new[] { "generate", "nick.co.za" });
